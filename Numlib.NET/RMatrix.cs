9fe0bf418c7b86a1c8519707401b49f775c5826a
bamine <amine.benhalloum@gmail.com>
2014-09-07 15:36:59 +0200

    Added real matrix struct

---
+++ Numlib.NET/RMatrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numlib.NET
+{
+    public struct RMatrix : ICloneable
+    {
+        private int nRows;
+        private int nCols;
+        private double[,] matrix;
+
+        public RMatrix(int nRows, int nCols)
+        {
+            this.nRows = nRows;
+            this.nCols = nCols;
+            this.matrix = new double[nRows, nCols];
+            for (int i = 0; i < nRows; i++)
+            {
+                for(int j=0;j<nCols;j++)
+                {
+                    matrix[i, j] = 0.0;
+                }
+            }
+        }
+
+        public RMatrix(double[,] matrix)
+        {
+            this.nRows = matrix.GetLength(0);
+            this.nCols = matrix.GetLength(1);
+            this.matrix = matrix;
+        }
+
+        public RMatrix(RMatrix m)
+        {
+            this.nRows = m.nRows;
+            this.nCols = m.nCols;
+            this.matrix = m.matrix;
+        }
+
+    }
+}

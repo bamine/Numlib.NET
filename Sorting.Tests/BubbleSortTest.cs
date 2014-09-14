246483e1c0b4285aecf6f92a3290c46d0b45b473
bamine <amine.benhalloum@gmail.com>
2014-09-14 18:13:22 +0200

    Added sorting tests

---
+++ Sorting.Tests/BubbleSortTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Sorting.Tests
+{
+    [TestClass]
+    public class BubbleSortTest
+    {
+        [TestMethod]
+        public void TestBubbleSort1()
+        {
+            int[] x = { 8, 6, 4, 7, 5, 3, 1, 9, 2 };
+            int[] sorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            BubbleSorter.Sort1(ref x);
+            Assert.IsTrue(x.SequenceEqual(sorted));
+        }
+
+        [TestMethod]
+        public void TestBubbleSort2()
+        {
+            int[] x = { 8, 6, 4, 7, 5, 3, 1, 9, 2 };
+            int[] sorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            BubbleSorter.Sort2(ref x);
+            Assert.IsTrue(x.SequenceEqual(sorted));
+        }
+
+        [TestMethod]
+        public void TestBubbleSort3()
+        {
+            int[] x = { 8, 6, 4, 7, 5, 3, 1, 9, 2 };
+            int[] sorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            BubbleSorter.Sort3(ref x);
+            Assert.IsTrue(x.SequenceEqual(sorted));
+        }
+
+        [TestMethod]
+        public void TestBubbleSort4()
+        {
+            int[] x = { 8, 6, 4, 7, 5, 3, 1, 9, 2 };
+            int[] sorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            BubbleSorter.Sort4(ref x);
+            Assert.IsTrue(x.SequenceEqual(sorted));
+        }
+    }
+}

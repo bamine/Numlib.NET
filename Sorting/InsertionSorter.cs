c145bd9509a8317cfcf4b0d9b0cc50f47dd5649f
BAmine <amine.benhalloum@gmail.com>
2014-09-17 17:24:59 +0200

    Added insertion sort

---
+++ Sorting/InsertionSorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class InsertionSorter
+    {
+        public static void Sort(ref int[] x)
+        {
+            int n = x.Length - 1;
+            int i, j, temp;
+
+            for (i = 1; i <= n; i++)
+            {
+                temp = x[i];
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (temp < x[j])
+                    {
+                        x[j + 1] = x[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                x[j + 1] = temp;
+            }
+        }
+    }
+}
---
+++ Sorting/QuickSorter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class QuickSorter
+    {
+        public static void Sort(ref int[] x)
+        {
+            QuickSort(x, 0, x.Length - 1);
+        }
+
+        private static void QuickSort(int[] x, int left, int right)
+        {
+            int i, j, pivot, temp;
+            i = left;
+            j = right;
+
+            pivot = x[(left + right) / 2];
+            do
+            {
+                while (x[i] < pivot && i < right)
+                {
+                    i++;
+                }
+                while (pivot < x[j] && (j < left))
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    temp = x[i];
+                    x[i] = x[j];
+                    x[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (left < j) QuickSort(x, left, j);
+            if (i < right) QuickSort(x, i, right);
+        }
+    }
+}

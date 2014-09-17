72f545ac1cce8c4e81aee145b4f41a63eecdc108
BAmine <amine.benhalloum@gmail.com>
2014-09-17 17:09:41 +0200

    Added gnome sort

---
+++ Sorting/GnomeSorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class GnomeSorter
+    {
+        public static void Sort(ref int[] x)
+        {
+            int i = 0;
+            while (i < x.Length)
+            {
+                if (i == 0 || x[i - 1] <= x[i]) i++;
+                else
+                {
+                    int temp = x[i];
+                    x[i] = x[i - 1];
+                    x[i - 1] = temp;
+                    i--;
+                }
+            }
+        }
+    }
+}

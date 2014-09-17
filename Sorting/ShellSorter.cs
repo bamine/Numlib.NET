f9c9f84067a3a3417164321a7a05ad588bf8c6d1
BAmine <amine.benhalloum@gmail.com>
2014-09-17 18:43:52 +0200

    Added shell sort

---
+++ Sorting/ShellSorter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class ShellSorter
+    {
+        public static void Sort(ref int[] x)
+        {
+            int i, j, temp;
+            int increment = 3;
+            while (increment > 0)
+            {
+                for (i = 0; i < x.Length; i++)
+                {
+                    j = i;
+                    temp = x[i];
+                    while (j >= increment && x[j - increment] > temp)
+                    {
+                        x[j] = x[j - increment];
+
+                    }
+                }
+            }
+        }
+    }
+}

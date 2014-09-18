c0203e1e166863f211e410d13bb927f1f00b3dea
BAmine <amine.benhalloum@gmail.com>
2014-09-18 18:23:38 +0200

    Added selection sort

---
+++ Sorting/SelectionSorter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class SelectionSorter
+    {
+        public static void Sort(ref int[] x)
+        {
+            int i, j, min, temp;
+            for (i = 0; i < x.Length - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < x.Length; j++)
+                {
+                    if (x[j] < x[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = x[i];
+                x[i] = x[min];
+                x[min] = temp;
+            }
+        }
+    }
+}

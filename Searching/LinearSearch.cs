1bbc2a2110f96aee993639d6b3be5dcd6495307e
bamine <amine.benhalloum@gmail.com>
2014-09-22 00:38:51 +0200

    Added linear search

---
+++ Searching/LinearSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    public class LinearSearch
+    {
+        public static int Search(int[] x, int valueToFind)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == valueToFind)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}

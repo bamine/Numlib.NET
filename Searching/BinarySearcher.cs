c89023d5fefcd5e231ebc02a3c93c19067b0ce02
bamine <amine.benhalloum@gmail.com>
2014-09-22 22:29:37 +0200

    Added binary search

---
+++ Searching/BinarySearcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    public class BinarySearcher
+    {
+        public static int Search(int[] x, int searchValue)
+        {
+            int left = 0;
+            int right = x.Length;
+            return binarySearch(x, searchValue, left, right);
+        }
+
+        private static int binarySearch(int[] x, int searchValue, int left, int right)
+        {
+            if (right < left)
+                return -1;
+            int mid = (left + right) / 2;
+            if (searchValue > x[mid])
+            {
+                return binarySearch(x, searchValue, mid + 1, right);
+            }
+            else if (searchValue < x[mid])
+            {
+                return binarySearch(x, searchValue, left, mid - 1);
+            }
+            else
+                return mid;
+        }
+    }
+}

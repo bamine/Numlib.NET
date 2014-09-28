bb764c5a4f910c08110f257364556bf8ef3fe3ea
bamine <amine.benhalloum@gmail.com>
2014-09-29 00:11:10 +0200

    Added Lagrange interpolation

---
+++ Interpolation/LagrangeInterpolation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+    public class LagrangeInterpolation
+    {
+        public static Func<double, double> BuildLagrangeInterpolator(double[] x, double[] y)
+        {
+            Func<double, double> interpolator = xval =>
+            {
+                double yval = 0.0;
+                double products = y[0];
+                for (int i = 0; i < x.Length; i++)
+                {
+                    products = y[i];
+                    for (int j = 0; j < x.Length; j++)
+                    {
+                        if (i != j)
+                        {
+                            products *= (xval - x[j]) / (x[i] - x[j]);
+                        }
+                    }
+                    yval += products;
+                }
+                return yval;
+            };
+
+            return interpolator;
+        }
+    }
+}

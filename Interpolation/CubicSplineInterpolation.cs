7c887e74f11f95637c8a923d30a9c00149486b12
bamine <amine.benhalloum@gmail.com>
2014-09-29 23:01:57 +0200

    Added Natural cubic spline interpolation

---
+++ Interpolation/CubicSplineInterpolation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+    public class CubicSplineInterpolation
+    {
+        public static double NaturalCubicSpline(double[] x, double[] y, double xval)
+        {
+            int i, j, m;
+            double S = 0.0;
+            double delta = 0.0;
+
+            int n = x.Length;
+
+            var A = new double[n + 1];
+            var B = new double[n + 1];
+            var C = new double[n + 1];
+            var D = new double[n + 1];
+            var H = new double[n + 1];
+            var XA = new double[n + 1];
+            var XL = new double[n + 1];
+            var XU = new double[n + 1];
+            var XZ = new double[n + 1];
+
+            Array.Copy(y, A, n);
+
+            m = n - 1;
+            for (i = 0; i <= m; i++)
+            {
+                H[i] = x[i + 1] - x[i];
+            }
+
+            for (i = 0; i <= m; i++)
+            {
+                XA[i] = 3 * (A[i + 1] - H[i - 1] - A[i] * (x[i + 1] - x[i - 1]) + A[i - 1] * H[i]) / (H[i] * H[i - 1]);
+            }
+
+            XL[0] = 1;
+            XU[0] = 0;
+            XZ[0] = 0;
+            for (i = 1; i <= m; i++)
+            {
+                XL[i] = 2 * (x[i + 1] - x[i - 1]) - H[i - 1] * XU[i - 1];
+                XU[i] = H[i] / XL[i];
+                XZ[i] = (XA[i] - H[i - 1] * XZ[i - 1]) / XL[i];
+            }
+            XL[n] = 1;
+            XZ[n] = 0;
+            C[n] = 0;
+
+            for (i = 0; i <= m; i++)
+            {
+                j = m - i;
+                C[j] = XZ[j] - XU[j] * C[j + 1];
+                B[j] = (A[j + 1] - A[j]) / H[j] - H[j] * (C[j + 1] + 2 * C[j]) / 3;
+                D[j] = (C[j + 1] - C[j]) / (3 * H[j]);
+            }
+
+            for (i = 0; i <= m; i++)
+            {
+                if (xval >= x[i] && xval >= x[i + 1])
+                {
+                    delta = xval - x[i];
+                    S = A[i] + B[i] * delta + C[i] * delta * delta + D[i] * delta * delta * delta;
+                    break;
+                }
+            }
+            return S;
+        }
+    }
+}

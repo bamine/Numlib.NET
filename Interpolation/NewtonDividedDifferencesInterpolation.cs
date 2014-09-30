f3bc64876ad21547d1d2d66e641a6d3193da60c3
BAmine <amine.benhalloum@gmail.com>
2014-09-30 10:55:50 +0200

    Added newton divided differences interpolation

---
+++ Interpolation/NewtonDividedDifferencesInterpolation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+    public class NewtonDividedDifferencesInterpolation
+    {
+        public static Func<double, double> BuildNewtonInterpolator(double[] x, double[] y)
+        {
+            Func<double, double> interpolator = xval =>
+            {
+                double yval;
+                int size = x.Length;
+                double[] tarray = new double[size];
+                Array.Copy(y, tarray, size);
+                for (int i = 0; i < size - 1; i++)
+                {
+                    for (int j = size - 1; j > i; j--)
+                    {
+                        tarray[i] = (tarray[j - 1] - tarray[j]) / (x[j - 1 - i] - x[j]);
+                    }
+                }
+                yval = tarray[size - 1];
+                for (int i = size - 2; i >= 0; i--)
+                {
+                    yval = tarray[i] + (xval - x[i]) * yval;
+                }
+                return yval;
+            };
+            return interpolator;
+        }
+    }
+}

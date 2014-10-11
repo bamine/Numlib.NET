d860a87f45743bdbc623494ec8278295a1b35b64
bamine <amine.benhalloum@gmail.com>
2014-10-11 19:34:35 +0200

    Added tests for matrix

---
+++ Structures.Tests/RMatrixTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Numlib.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures.Tests
+{
+    [TestClass]
+    public class RMatrixTests
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var A = new RMatrix(new double[3,3] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+            var B = new RMatrix(new double[3, 3] { { 1, 2, 3 }, { 1, 1, 1 }, { 1, 2, 1 } });
+            var actual = A * B;
+            var expected = B;
+            Assert.IsTrue(actual == expected);
+        }
+    }
+}

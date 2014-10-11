1b18cb2f2a5e788c62bf960896c40ddd986f391f
bamine <amine.benhalloum@gmail.com>
2014-10-11 23:30:57 +0200

    Gauss seidel iteration

--- LinearEquations/Iteration.cs
+++ LinearEquations/Iteration.cs
@@ -11,6 +11,7 @@ namespace LinearEquations
     public class Iteration
     {
         const double epsilon = 1.0e-500;
+
         public static RVector GaussJacobi(RMatrix A, RVector b, int maxIterations, double tolerance)
         {
             int bSize = b.GetVectorSize;
@@ -32,6 +33,40 @@ namespace LinearEquations
                         {
                             entry -= A[i, j] * xOld[j];
                         }
+                    }
+                    x[i] = entry / diagonal;
+                }
+                var dx = x - xOld;
+                if (dx.GetNorm() < tolerance)
+                {
+                    return x;
+                }
+            }
+            return x;
+        }
+
+        public static RVector GaussSeidel(RMatrix A, RVector b, int maxIterations, double tolerance)
+        {
+            var size = b.GetVectorSize;
+            var x = new RVector(size);
+            for (int iter = 0; iter < maxIterations; iter++)
+            {
+                RVector xOld = x.Clone();
+                for (int i = 0; i < size; i++)
+                {
+                    var entry = b[i];
+                    var diagonal = A[i, i];
+                    if (Math.Abs(diagonal) < epsilon)
+                    {
+                        throw new DivideByZeroException("Diagonal elemnts are too small to normalize");
+                    }
+                    for (int j = 0; j < i; j++)
+                    {
+                        entry -= A[i, j] * x[j];
+                    }
+                    for (int j = i + 1; j < size; j++)
+                    {
+                        entry -= A[i, j] * xOld[j];
                     }
                     x[i] = entry / diagonal;
                 }

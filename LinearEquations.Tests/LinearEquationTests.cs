ff14a7ebce36933d5346e2fb746e0a691b45f319
bamine <amine.benhalloum@gmail.com>
2014-10-11 23:31:45 +0200

    Renamed test file

--- LinearEquations.Tests/LinearEquationTests.cs
+++ LinearEquations.Tests/LinearEquationTests.cs
@@ -6,7 +6,7 @@ using Numlib.NET.Structures;
 namespace LinearEquations.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class LinearEquationTests
     {
         [TestMethod]
         public void TestGaussJordan()

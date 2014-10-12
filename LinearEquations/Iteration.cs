32532190fe13fe873030b95dc91669b055de3d1d
bamine <amine.benhalloum@gmail.com>
2014-10-12 02:59:41 +0200

    jacobi eigne value and vector finder

--- LinearEquations/Iteration.cs
+++ LinearEquations/Iteration.cs
@@ -23,7 +23,7 @@ namespace LinearEquations
                 {
                     var entry = b[i];
                     var diagonal = A[i, i];
-                    if(Math.Abs(diagonal)<epsilon)
+                    if (Math.Abs(diagonal) < epsilon)
                     {
                         throw new DivideByZeroException("Diagonal elemnts are too small to normalize");
                     }
@@ -77,6 +77,233 @@ namespace LinearEquations
                 }
             }
             return x;
+        }
+
+        public static void JacobiEigen(RMatrix A, int maxsize, int n, double epsilon, out RMatrix eigenval, out RMatrix eigenvec)
+        {
+            int i, j, p, q, flag;
+            var d = new RMatrix(n, n);
+            var s = new RMatrix(n, n);
+            var s1 = new RMatrix(n, n);
+            var s1t = new RMatrix(n, n);
+            var temp = new RMatrix(n, n);
+            double theta, max;
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    d[i, j] = A[i, j];
+                    if (i == j)
+                    {
+                        s[i, j] = 1.0;
+                    }
+                    else
+                    {
+                        s[i, j] = 0.0;
+                    }
+                }
+            }
+
+            flag = 1;
+            while (flag == 1)
+            {
+                flag = 0;
+                i = 0;
+                j = 1;
+                max = Math.Abs(d[0, 1]);
+                for (p = 0; p < n; p++)
+                {
+                    for (q = 0; q < n; q++)
+                    {
+                        if (p != q)
+                        {
+                            if (max < Math.Abs(d[p, q]))
+                            {
+                                max = Math.Abs(d[p, q]);
+                                i = p;
+                                j = q;
+                            }
+                        }
+                    }
+                }
+
+                if (d[i, i] == d[j, j])
+                {
+                    if (d[i, j] > 0)
+                    {
+                        theta = Math.PI / 4.0;
+                    }
+                    else
+                    {
+                        theta = -Math.PI / 4.0;
+                    }
+                }
+                else
+                {
+                    theta = 0.5 * Math.Atan(2.0 * d[i, j] / (d[i, i] - d[j, j]));
+                    if (Double.IsNaN(theta))
+                    {
+                        break;
+                    }
+                }
+
+                for (p = 0; p < n; p++)
+                {
+                    s1[p, p] = 1.0;
+                    s1t[p, p] = 1.0;
+                }
+
+                s1[i, i] = Math.Cos(theta);
+                s1[j, j] = s1[i, i];
+                s1[j, i] = Math.Sin(theta);
+                s1[i, j] = -s1[j, i];
+
+                s1t[i, i] = s1[i, i];
+                s1t[j, j] = s1[j, j];
+                s1t[i, j] = s1[j, i];
+                s1t[j, i] = s1[i, j];
+
+                d = s1t * d * s1;
+                s = s * s1;
+
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        if (j != i)
+                        {
+                            if (Math.Abs(d[i, j]) > epsilon)
+                            {
+                                flag = 1;
+                            }
+                        }
+                    }
+                }
+            };
+            eigenval = d;
+            eigenvec = s;
+        }
+
+        public static void JacobiEigenValVec(RMatrix a, int maxsize, int n, double epsilon, out RMatrix eigenval, out RMatrix eigenvec)
+        {
+            int i, j, p, q, flag;
+            double[,] d = new double[maxsize, maxsize];
+            double[,] s = new double[maxsize, maxsize];
+            double[,] s1 = new double[maxsize, maxsize];
+            double[,] s1t = new double[maxsize, maxsize];
+            double[,] temp = new double[maxsize, maxsize];
+            double theta, max;
+            //Initialization of matrix d and s
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    d[i, j] = a[i, j];
+                    s[i, j] = 0.0;
+                }
+            }
+            for (i = 0; i < n; i++) s[i, i] = 1.0;
+            do
+            {
+                flag = 0;
+                //Find largest off-diagonal element
+                i = 0;
+                j = 1;
+                max = Math.Abs(d[0, 1]);
+                for (p = 0; p < n; p++)
+                {
+                    for (q = 0; q < n; q++)
+                    {
+                        if (p != q) //off diagonal element
+                        {
+                            if (max < Math.Abs(d[p, q]))
+                            {
+                                max = Math.Abs(d[p, q]);
+                                i = p;
+                                j = q;
+                            }
+                        }
+                    }
+                }
+                if (d[i, i] == d[j, j])
+                {
+                    if (d[i, j] > 0) theta = Math.PI / 4.0;
+                    else theta = -Math.PI / 4.0;
+                }
+                else
+                {
+                    theta = 0.5 * Math.Atan(2.0 * d[i, j] / (d[i, i] - d[j, j]));
+                }
+                //Construction of the matrix s1 and s1t
+                for (p = 0; p < n; p++)
+                {
+                    for (q = 0; q < n; q++)
+                    {
+                        s1[p, q] = 0.0;
+                        s1t[p, q] = 0.0;
+                    }
+                }
+                for (p = 0; p < n; p++)
+                {
+                    s1[p, p] = 1.0;
+                    s1t[p, p] = 1.0;
+                }
+                s1[i, i] = Math.Cos(theta); s1[j, j] = s1[i, i];
+                s1[j, i] = Math.Sin(theta); s1[i, j] = -s1[j, i];
+                s1t[i, i] = s1[i, i]; s1t[j, j] = s1[j, j];
+                s1t[i, j] = s1[j, i]; s1t[j, i] = s1[i, j];
+                //Product of s1t and d
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        temp[i, j] = 0.0;
+                        for (p = 0; p < n; p++)
+                            temp[i, j] += s1t[i, p] * d[p, j];
+                    }
+                }
+                //Product of temp and s1: d = s1t * d * s1
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        d[i, j] = 0.0;
+                        for (p = 0; p < n; p++)
+                            d[i, j] += temp[i, p] * s1[p, j];
+                    }
+                }
+                //Product of s and s1: s = s*s1
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        temp[i, j] = 0.0;
+                        for (p = 0; p < n; p++)
+                            temp[i, j] += s[i, p] * s1[p, j];
+                    }
+                }
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        s[i, j] = temp[i, j];
+                    }
+                }
+                //check to see if d is a diagonal matrix
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        if (i != j)
+                            if (Math.Abs(d[i, j]) > epsilon)
+                                flag = 1;
+                    }
+                }
+            } while (flag == 1);
+            //copy results to output matrices
+            eigenval = new RMatrix(d);
+            eigenvec = new RMatrix(s);
         }
     }
 }
